b063b395eb46d45a8815fb6309c1977d1e714329
Gilang Saeful Anwar <sagilang@gmail.com>
2019-07-07 15:59:01 +0700

    Templating
    Mengubah menu-menu bawaan dari template ke menu yang direncanakan akan ada di aplikasi.

--- admin/application/views/templates/header.php
+++ admin/application/views/templates/header.php
@@ -14,8 +14,8 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="<?= base_url() ?>" class="brand-link">
-                <img src="<?= base_url('assets/dist/img/AdminLTELogo.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-bolder text-info">DP<span class="text-primary">PKBP</span>3A</span>
+                <img src="<?= base_url('assets/dist/img/Logo_Kota_Tasikmalaya.png') ?>" alt="Logo Kota Tasik" class="brand-image elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-bolder text-white">DPPKBP3A</span>
             </a>
 
             <?php $this->load->view('templates/_partials/sidebar') ?>

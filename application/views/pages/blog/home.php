5ca0c67486e716166396ed77421a1ea9843f3765
lusyfadilah <lusyfadilah@users.noreply.github.com>
2019-08-17 23:19:14 +0700

    create footer blog

--- application/views/pages/blog/home.php
+++ application/views/pages/blog/home.php
@@ -207,6 +207,26 @@
         </div>
     </div>
 
+    <!-- FOOTER START -->
+    <div class="footer">
+        <div class="footer-social-icon">
+            <ul>
+                <li><a href="#" target="blank"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#" target="blank"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#" target="blank"><i class="fab fa-google-plus"></i></a></li>
+                <li><a href="#" target="blank"><i class="fab fa-youtube"></i></a></li>
+
+            </ul>
+        </div>
+        <div class="copyright">
+            <p><i class="far fa-copyright"></i> Dinas PPKBP3A 2019</i></p>
+
+
+        </div>
+
+    </div>
+    <!-- FOOTER END -->
+
     <!-- jQuery -->
     <script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
     <!-- jQuery UI 1.11.4 -->

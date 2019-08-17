5453a918b1b26aefa90063f442088cf755f2999f
Gilang Saeful Anwar <sagilang@gmail.com>
2019-08-17 22:13:27 +0700

    Update user.php

--- application/views/pages/user.php
+++ application/views/pages/user.php
@@ -38,8 +38,8 @@
                                     <td><?= $row->dibuat_pada ?></td>
                                     <td>
                                         <a href="<?= base_url() ?>" class="btn btn-sm text-success" data-username="<?= $row->username ?>" data-email="<?= $row->email ?>" data-nama="<?= $row->nama ?>" data-role="<?= $row->role ?>" data-created="<?= $row->dibuat_pada ?>"><i class="fas fa-eye"></i> Lihat</a>
-                                        <a href="<?= base_url() ?>" class="btn btn-sm text-primary" data-id="<?= $listuser->id ?>" data-username="<?= $row->username ?>" data-email="<?= $row->email ?>" data-nama="<?= $row->nama ?>" data-role="<?= $row->role ?>" data-created="<?= $row->dibuat_pada ?>"><i class="fas fa-edit"></i> Edit</a>
-                                        <a href="<?= base_url('user/delete/') . $listuser->id ?>" class="btn btn-sm text-danger"><i class="fas fa-trash-alt"></i> Hapus</a>
+                                        <a href="<?= base_url() ?>" class="btn btn-sm text-primary" data-id="<?= $row->id ?>" data-username="<?= $row->username ?>" data-email="<?= $row->email ?>" data-nama="<?= $row->nama ?>" data-role="<?= $row->role ?>" data-created="<?= $row->dibuat_pada ?>"><i class="fas fa-edit"></i> Edit</a>
+                                        <a href="<?= base_url('user/delete/') . $row->id ?>" class="btn btn-sm text-danger"><i class="fas fa-trash-alt"></i> Hapus</a>
                                     </td>
                                 </tr>
                                 <?php endforeach; ?>
